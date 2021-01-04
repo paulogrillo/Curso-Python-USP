67eeee6501cc616c50e11e3705488ebb0b83d0f7
Paulo <paulodigitalart@gmail.com>
2021-01-04 14:47:38 -0300

    update

--- introdução-ao-python/Condicionais/raizQuadrada.py
+++ introdução-ao-python/Condicionais/raizQuadrada.py
@@ -2,20 +2,22 @@
 import math #Import math
 
 #Entrada de dados
-a = int(input("Digite um valor para A: "))
-b = int(input("Digite um valor para B: "))
-c = int(input("Digite um valor para C: "))
+a = float(input("Digite um valor para A: "))
+b = float(input("Digite um valor para B: "))
+c = float(input("Digite um valor para C: "))
 
 #Calculo do delta
-delta = b*b - 4 * a * c
+delta = b ** 2 - 4 * a * c
 
 #Condicionais
-if delta < 0:
-    print("A equação não possui raizes reais.")
-elif delta == 0:
-    raiz = (-1*b + math.sqrt(delta))/(2 * a)
-    print("A equacao possui apenas uma raiz que e ",raiz)
-elif delta > 0:
-    raiz1 =(-1*b + math.sqrt(delta))/(2 * a)
-    raiz2 =(-1*b + math.sqrt(delta))/(2 * a)
-    print("As raizes da equacao sao ",raiz1, "e",raiz2)
+if delta == 0:
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    print("A única raiz é: ", raiz1)
+else:
+    if delta < 0:
+        print("Essa equação não possui raízes reais")
+    else:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        print("A primeira raiz é: ", raiz1)
+        print("A segunda raiz é: ", raiz2)
